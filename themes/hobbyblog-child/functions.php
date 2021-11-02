0c70227f6253e6aff33c93fafdcaf65a408a3825
Michelle <Michelle.Bartley@dcmail.ca>
2021-11-02 04:37:27 -0400

    Create new child theme

---
+++ themes/hobbyblog-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+add_action( 'wp_enqueue_scripts', 'hobbyblogchild_wp_enqueue_scripts' );
+function hobbyblogchild_wp_enqueue_scripts() {
+    wp_enqueue_style( 
+        'child-style', 
+        get_stylesheet_uri(),
+        array( 'parenthandle' ), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
+}
