4ec146f371e7b4a23b5e32ce2d90a9ba57ae50f0
Michelle <Michelle.Bartley@dcmail.ca>
2021-12-07 13:46:56 -0500

    functions php
    updated function page to include new css file

--- themes/hobbyblog-child/functions.php
+++ themes/hobbyblog-child/functions.php
@@ -9,12 +9,26 @@
  * @since Twenty Twenty-One 1.0
  */
 
+
 function hobbyblogchild_wp_enqueue_scripts() {
     wp_enqueue_style( 
+        'parent-style', 
+        get_stylesheet_directory_uri().'./style.css'
+    );
+    wp_enqueue_style( 
         'hobbyblogchild-style', 
-        get_stylesheet_uri(),
-        array( 'twenty-twenty-one-style' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+        get_stylesheet_directory_uri().'./css/style.css'
     );
 }
 add_action( 'wp_enqueue_scripts', 'hobbyblogchild_wp_enqueue_scripts' );
+
+// function hobbyblogchild_wp_enqueue_scripts() {
+//     wp_enqueue_style( 
+//         'hobbyblogchild-style', 
+//         get_stylesheet_uri(),
+//         array( 'twenty-twenty-one-style' ), 
+//         wp_get_theme()->get('Version')
+//     );
+// }
+// add_action( 'wp_enqueue_scripts', 'hobbyblogchild_wp_enqueue_scripts' );
+?>
